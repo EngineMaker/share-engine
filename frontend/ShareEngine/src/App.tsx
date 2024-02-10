b0e1f2733623cba46c123d7dfb9f18f5553d32df
shima8823 <taiyaki1275@gmail.com>
2024-02-11 07:11:26 +0900

    feat-front: colored nav bar icon

--- frontend/ShareEngine/src/App.tsx
+++ frontend/ShareEngine/src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PublishScreen from './screens/PublishScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import { DetailsScreen } from './screens/DetailScreen';
+import {DetailsScreen} from './screens/DetailScreen';
 import LoginScreen from './screens/LoginScreen';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {Image, Text} from 'react-native';
 
 const MainStack = createNativeStackNavigator();
 const LoginStack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 const HomeStack = createNativeStackNavigator();
+
+const homeIcon = require('./images/homeIcon.png');
+const publishIcon = require('./images/cameraIcon.png');
+const profileIcon = require('./images/profileIcon.png');
+const selectedHomeIcon = require('./images/selectedHomeIcon.png');
+const selectedPublishIcon = require('./images/selectedCameraIcon.png');
+const selectedProfileIcon = require('./images/selectedProfileIcon.png');
 
 const LoginStackScreen = () => (
   <LoginStack.Navigator initialRouteName="Login">
@@ -44,17 +52,50 @@ const HomeTabScreen = () => (
     <Tab.Screen
       name="HomeStack"
       component={HomeStackScreen}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={focused ? selectedHomeIcon : homeIcon}
+            style={{width: 24, height: 24}}
+          />
+        ),
+        tabBarLabel: ({focused}) => (
+          <Text style={{color: focused ? '#5054fc' : '#9c9c9d'}}>Home</Text>
+        ),
+      }}
     />
     <Tab.Screen
       name="Publish"
       component={PublishScreen}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={focused ? selectedPublishIcon : publishIcon}
+            style={{width: 24, height: 24}}
+          />
+        ),
+        tabBarLabel: ({focused}) => (
+          <Text style={{color: focused ? '#5054fc' : '#9c9c9d'}}>Publish</Text>
+        ),
+      }}
     />
     <Tab.Screen
       name="Profile"
       component={ProfileScreen}
-      options={{ headerShown: false }}
+      options={{
+        headerShown: false,
+        tabBarIcon: ({focused}) => (
+          <Image
+            source={focused ? selectedProfileIcon : profileIcon}
+            style={{width: 24, height: 24}}
+          />
+        ),
+        tabBarLabel: ({focused}) => (
+          <Text style={{color: focused ? '#5054fc' : '#9c9c9d'}}>Profile</Text>
+        ),
+      }}
     />
   </Tab.Navigator>
 );
