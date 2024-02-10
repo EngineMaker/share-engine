8c076c1c4947ab7bcde6fa3e4e9a122608b7e6f3
yukisato <yuki01377@gmail.com>
2024-02-10 17:16:05 +0900

    dbの接続情報を追加

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -7,7 +7,23 @@ variable "region" {
   default = "asia-northeast1"
 }
 
-variable "db_password" {
-  description = "Database user password"
+variable "db_conn" {
+  description = "Database connection info"
   type        = string
 }
+
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Database user"
+  type        = string
+}
+
